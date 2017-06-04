5fbc71fdd922c3cbc6591d577dba219e5b9d8c8b
Javier <richards.javier@gmail.com>
2017-06-04 15:05:48 -0400

    Prueba

--- app/src/main/java/com/example/richa/kuromasu3/MainActivity.java
+++ app/src/main/java/com/example/richa/kuromasu3/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.richa.kuromasu3;
 
+import android.content.res.AssetManager;
 import android.graphics.Color;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,18 +18,24 @@ import android.widget.Toast;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    int[][]juego = {{1,2,2}, {1, 1, 1}};
 
-    EditText et;
-    String archivoBytes = "archivoBytes.txt";
-    String archivoDatos = "archivoDatos.txt";
+
+    //gris=0,negro=1, blanco=2
+
+    public MainActivity() throws IOException {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +43,9 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int filas=8;
-        int columnas=8;
-        final int matriz[][]= new int[20][20];
+        int filas=2;
+        int columnas=3;
+        final int matriz[][]= new int[2][3];
 
 
         LinearLayout layoutPrincipal=(LinearLayout) this.findViewById(R.id.principal);
@@ -53,12 +62,17 @@ public class MainActivity extends AppCompatActivity {
                 final int p = j;
                 final int k=i;
                 final Button b= new Button(this);
-
+                b.setText(String.valueOf(juego[i][j]));
                 matriz[i][j]=0;
+
 
                 b.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+
+                        System.out.println("hola2");
+                        imprime(matriz);
+                        if(comprobar(matriz)) System.out.println("Completo");
 
                         if(matriz[k][p]==0){
                         b.setBackgroundColor(Color.BLACK);
@@ -66,14 +80,37 @@ public class MainActivity extends AppCompatActivity {
                         }
                         else if(matriz[k][p]==1){
 
-                            b.setBackgroundColor(Color.RED);
+                            b.setBackgroundColor(Color.WHITE);
                             matriz[k][p]++;
                         }
                         else {b.setBackgroundColor(Color.GRAY); matriz[k][p]=0;}
 
 
+
+                       if(Arrays.deepEquals(juego, matriz)){Toast toast1 =
+                               Toast.makeText(getApplicationContext(),
+                                       "Felicitaciones", Toast.LENGTH_SHORT);
+
+                           toast1.show();
+                       System.out.println("correcto");
+                       }
+                       else System.out.println("Incorrecto");
+
+
+
+/*
+                        for(int h=0;h<2;h++)    {
+                            for(int k=0;k<3;k++){System.out.println("matriz 1"+matriz[k][h]+"matriz2"+juego[k][h]);}
+
+
+
+
+
+                        }*/
+
                     }
                 });
+
 
 // y se añade al layout
                 layout.addView(b);}
@@ -86,54 +123,36 @@ public class MainActivity extends AppCompatActivity {
 //Termina el Main Activity
 
 
-    public void onClickLoadData(View view) {
-        try {
-            FileInputStream fis = openFileInput(archivoDatos);
-            DataInputStream dis = new DataInputStream(fis);
-            Byte b = dis.readByte();
-            Character c = dis.readChar();
-            Double d = dis.readDouble();
-            Integer i = dis.readInt();
-            Long l = dis.readLong();
-            Float f = dis.readFloat();
-            String su = dis.readUTF();
-            DisplayToast(b.toString() + " " + c.toString() + " " + d.toString() + " "
-                    + i.toString() + l.toString() + " " + f.toString() + " " + su);
-            dis.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+boolean comprobar(int matriz2[][]) {
+    for (int i = 0; i < 2; i++) {    // El primer índice recorre las filas.
+        for (int j = 0; j < 3; j++) {    // El segundo índice recorre las columnas.
+            // Procesamos cada elemento de la matriz.
+
+            if (matriz2[i][j]==0) return false;
     }
-
-
-
-    public void onClickSaveData(View view) {
-        try {
-            FileOutputStream fos = openFileOutput(archivoDatos, MODE_PRIVATE);
-            DataOutputStream dos = new DataOutputStream(fos);
-            dos.writeByte(10);
-            dos.writeChar('A');
-            dos.writeDouble(3.14159);
-            dos.writeInt(123);
-            dos.writeLong(1234567890);
-            dos.writeFloat(3.1415926F);
-            dos.writeUTF("Esto es UTF");
-            dos.flush();
-            dos.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        DisplayToast("Escribió datos en archivo");
-    }
-
-    private void DisplayToast(String mensaje) {
-        Toast.makeText(getBaseContext(), mensaje, Toast.LENGTH_SHORT).show();
-    }
-
-
 
 }
+
+return true;}
+
+
+    void imprime(int matriz2[][]) {
+        System.out.println("hola");
+        for (int i = 0; i < 2; i++) {    // El primer índice recorre las filas.
+            for (int j = 0; j < 3; j++) {    // El segundo índice recorre las columnas.
+                // Procesamos cada elemento de la matriz.
+                System.out.println("el numero es"+matriz2[i][j]);
+
+            }
+
+        }
+        }
+
+
+    }
+
+
+
+
+
+
